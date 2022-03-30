8ce2bd91532734064d8577b9685608c423f96e46
jokerByrant <sxhjoker@163.com>
2022-03-30 15:53:48 +0800

    #91

--- src/main/java/com/sxh/interview/leetcode/dp/_91_jie_ma_fang_fa.java
+++ src/main/java/com/sxh/interview/leetcode/dp/_91_jie_ma_fang_fa.java
@@ -10,6 +10,27 @@ public class _91_jie_ma_fang_fa {
         // 若最后两个数字组合大于26，那么 dp[i] = dp[i - 1]
         // 若最后两个数字组合小于等于26，那么 dp[i] = dp[i - 1] + 1
         int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        int pre = 0, cur;
+        for (int i = 1; i <= n; ++i) {
+            cur = Character.getNumericValue(s.charAt(i - 1));
+            if (cur != 0) {
+                dp[i] += dp[i - 1];
+            }
+            if (i > 1 && (pre == 1 || (pre == 2 && cur <= 6))) {
+                dp[i] += dp[i - 2];
+            }
+            pre = cur;
+        }
+        return dp[n];
+    }
+    // 优化版，数组替换为变量
+    public static int numDecodings2(String s) {
+        // 转移方程：设dp[i]为长度为i的字符包含的解码方法总数
+        // 若最后两个数字组合大于26，那么 dp[i] = dp[i - 1]
+        // 若最后两个数字组合小于等于26，那么 dp[i] = dp[i - 1] + 1
+        int n = s.length();
         int pre = 0, cur = 1, tmp = 0;
         for (int i = 1; i <= n; ++i) {
             tmp = 0;
